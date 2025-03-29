c67ed761cebc146d3bf6798a5afb1ab1487372ad
EwanGreer <ewanja.greer@gmail.com>
2025-03-29 13:57:32 +0000

    feat: remove nvim

--- .config/nvim/lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
--- Oil
-vim.keymap.set("n", "-", "<cmd>Oil --float<cr>", { desc = "Open parent directory" })
