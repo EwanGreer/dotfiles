c788be07a361c6493f5c5009717386d11bb0b0a8
egreerdp <ewan.greer@dailypay.com>
2025-08-03 16:22:06 +0100

    feat(scripts): gum sctips

--- .config/scripts/find-file.sh
+++ .config/scripts/find-file.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-for cmd in fd fzf-tmux bat nvim; do
+for cmd in fd fzf-tmux bat nvim gum; do
   if ! command -v "$cmd" &>/dev/null; then
     echo "Error: '$cmd' is not installed." >&2
     exit 1
