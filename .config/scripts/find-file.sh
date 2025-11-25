f2890b238f175b215c614c83e7f10cf7f022a2b4
egreerdp <ewan.greer@dailypay.com>
2025-11-25 11:17:48 +0000

    fix(scripts): update find-file

--- .config/scripts/find-file.sh
+++ .config/scripts/find-file.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-for cmd in fd fzf-tmux bat nvim gum; do
+for cmd in fd fzf bat nvim gum; do
   if ! command -v "$cmd" &>/dev/null; then
     echo "Error: '$cmd' is not installed." >&2
     exit 1
@@ -10,7 +10,7 @@ done
 vf() {
   local file
   file=$(fd --hidden --type f --type d --exclude .git 2>/dev/null | \
-         fzf-tmux -p 80%,60% --preview '[[ -f {} ]] && bat --style=numbers --color=always --line-range=:500 {} || ls -la {}')
+         fzf --tmux 80%,60% --preview '[[ -f {} ]] && bat --style=numbers --color=always --line-range=:500 {} || ls -la {}')
   
   [[ -z "$file" ]] && return
 
