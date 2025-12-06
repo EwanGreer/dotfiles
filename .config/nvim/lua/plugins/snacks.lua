a4923c4ddc955d716266f52c27ce3bd1f43dcaec
egreerdp <ewan.greer@dailypay.com>
2025-12-06 16:01:58 +0000

    feat(nvim): update dashboard

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -5,73 +5,46 @@ return {
     dashboard = {
       preset = {
         keys = {
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          {
-            icon = " ",
-            key = "c",
-            desc = "Config",
-            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-          },
-          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-          {
-            icon = " ",
-            desc = "Browse Repo",
-            key = "b",
-            action = function()
-              Snacks.gitbrowse()
-            end,
-          },
-          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          { key = "f", action = ":lua Snacks.dashboard.pick('files')" },
+          { key = "n", action = ":ene | startinsert" },
+          { key = "g", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { key = "r", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { key = "c", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { key = "q", action = ":qa" },
         },
       },
       sections = {
-        { padding = 3 },
-        { pane = 1, icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-        { pane = 1, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-        { pane = 1, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-        function()
-          local in_git = Snacks.git.get_root() ~= nil
-          local cmds = {
-            {
-              icon = " ",
-              title = "Git Status",
-              cmd = "git --no-pager diff --stat -B -M -C",
-              height = 5,
-            },
-          }
-          return vim.tbl_map(function(cmd)
-            return vim.tbl_extend("force", {
-              section = "terminal",
-              enabled = in_git,
-              ttl = 5 * 60,
-              indent = 3,
-            }, cmd)
-          end, cmds)
-        end,
-        { pane = 1, section = "startup" },
+        { section = "header" },
+        {
+          section = "terminal",
+          cmd = "git --no-pager diff --stat -B -M -C",
+          height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+        },
+        {
+          icon = " ",
+          title = "Recent Files",
+          section = "recent_files",
+          indent = 2,
+          padding = 1,
+          limit = 5,
+        },
+        { section = "startup" },
       },
     },
     picker = {
       sources = {
-        files = {
-          hidden = true,
-        },
+        files = { hidden = true },
         explorer = {
           hidden = true,
-          layout = {
-            preset = "sidebar",
-            layout = {
-              position = "right",
-            },
-          },
+          layout = { preset = "sidebar", layout = { position = "right" } },
         },
         grep = { hidden = true },
-        grep_word = { hidden = true },
-        grep_buffers = { hidden = true },
       },
     },
   },
