6f69be65e95964dc7e7b9d413d62ac909de8f6a3
EwanGreer <ewanja.greer@gmail.com>
2026-01-19 20:14:59 +0000

    fix(nvim): better blink config

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -3,13 +3,16 @@ return {
   opts = {
     sources = {
       providers = {
-        lsp = { min_keyword_length = 2 },
-        buffer = { min_keyword_length = 3 },
-        snippets = { min_keyword_length = 2 },
+        lsp = { min_keyword_length = 0 },
+        buffer = { min_keyword_length = 1 },
+        snippets = { min_keyword_length = 1 },
       },
     },
     completion = {
       list = { selection = { preselect = false, auto_insert = false } },
+    },
+    keymap = {
+      ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
     },
   },
 }
