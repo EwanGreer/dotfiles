0c49e4fe8f1faf3c3c9afca846f656c48d2d6741
EwanGreer <ewanja.greer@gmail.com>
2026-04-18 16:01:23 +0100

    fix: remove directory from find file script

--- .config/scripts/find-file.sh
+++ .config/scripts/find-file.sh
@@ -9,7 +9,7 @@ done
 
 vf() {
   local file
-  file=$(fd --hidden --type f --type d --exclude .git 2>/dev/null | \
+  file=$(fd --hidden --type f --exclude .git 2>/dev/null | \
          fzf --tmux 80%,60% --preview '[[ -f {} ]] && bat --style=numbers --color=always --line-range=:500 {} || ls -la {}')
   
   [[ -z "$file" ]] && return
