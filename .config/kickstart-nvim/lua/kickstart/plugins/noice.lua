828158de3e53bf76e7ffcae3a2a0cd7862dbb6d1
EwanGreer <ewanja.greer@gmail.com>
2026-06-15 21:23:48 +0100

    feat: suppress db connection error msg

--- .config/kickstart-nvim/lua/kickstart/plugins/noice.lua
+++ .config/kickstart-nvim/lua/kickstart/plugins/noice.lua
@@ -26,6 +26,18 @@ require('noice').setup {
     long_message_to_split = true,
     lsp_doc_border = true,
   },
+  routes = {
+    -- suppress dadbod connection errors when the db is unreachable
+    {
+      filter = { event = 'msg_show', find = 'DB exec error' },
+      opts = { skip = true },
+    },
+    {
+      filter = { event = 'msg_show', find = '^DB:' },
+      opts = { skip = true },
+    },
+  },
 }
 
+vim.keymap.set('n', '<leader>un', '<cmd>NoiceDismiss<CR>', { desc = '[U]I dismiss [N]otifications' })
 vim.keymap.set('n', '<leader>sN', '<cmd>Telescope notify<CR>', { desc = '[S]earch [N]otifications' })
